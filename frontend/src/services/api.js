1e3dbf8da807988a3f4aee0d452be4ed486bf566
Ermiyas Daniel <ermiyasdaniel619@gmail.com>
2026-06-11 04:16:00 -0800

    insert an additional base URL for development.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://task-mangment-dashbord-rbx1.vercel.app/api",
+  // baseURL: "http://localhost:5000/api",// for local development
   withCredentials: true, // Instructs browser to attach cookies to every request automatically
 });
 
